c848719f4a5e8bd9294800d6e31168cfddb3e977
JoshuaHolloway <jhollow6@asu.edu>
2022-01-27 22:15:12 -0600

    integrated socket.io with node server ready for endponits

--- backend/app copy.js
+++ backend/app copy.js
@@ -1,15 +1,6 @@
 // "socket.io": "^2.3.0"
-// const io = require('socket.io')(5000);
+const io = require('socket.io')(5000);
 // "socket.io-client": "^2.3.0"
-
-const httpServer = require('http').createServer((req, res) => {
-  // serve the index.html file
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Content-Length', Buffer.byteLength(content));
-  res.end(content);
-});
-
-const io = require('socket.io')(httpServer);
 
 // the count state
 let count = 0;
@@ -27,7 +18,4 @@ io.on('connect', function (socket) {
     // emit to EVERYONE the updated count
     io.emit('counter updated', count);
   });
-});
-httpServer.listen(5000, () => {
-  console.log('go to http://localhost:5000');
 });
